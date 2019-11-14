451e76b40b2ba7a77110bd35839cb73b5974b3f7
sakurafurafoop <saku.bel.5724@gmail.com>
2019-11-14 23:46:49 +0900

    Plusボタンのダイアログ苦戦中

--- app/src/main/java/app/sakura/momonga/kisotaionkanri/MainActivity.kt
+++ app/src/main/java/app/sakura/momonga/kisotaionkanri/MainActivity.kt
@@ -1,7 +1,10 @@
 package app.sakura.momonga.kisotaionkanri
 
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,6 +13,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        plusButton.setOnClickListener {
+
+            val builder: AlertDialog.Builder? = this?.let {
+                AlertDialog.Builder(this)
+
+            }
+            builder?.apply {
+                setPositiveButton("ok",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        // User clicked OK button
+                    })
+                setNegativeButton("no",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        // User cancelled the dialog
+                    })
+            }
+
+            builder?.setMessage("やっほ")
+
+
+            val dialog: AlertDialog? = builder?.create()
+
+        }
 
         //NavigationViewを選択した時
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
