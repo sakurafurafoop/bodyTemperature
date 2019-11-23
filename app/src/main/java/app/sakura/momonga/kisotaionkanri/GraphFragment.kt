4637dd7d85c6e23545c0f02424a3a234eafb2761
sakurafurafoop <safura_nnloop0927@yahoo.co.jp>
2019-11-23 22:49:12 +0900

    グラフが反映される

--- app/src/main/java/app/sakura/momonga/kisotaionkanri/GraphFragment.kt
+++ app/src/main/java/app/sakura/momonga/kisotaionkanri/GraphFragment.kt
@@ -1,27 +1,108 @@
 package app.sakura.momonga.kisotaionkanri
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Typeface
 import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import io.realm.Realm
+import kotlinx.android.synthetic.main.fragment_graph.*
 
 class GraphFragment : Fragment() {
 
-
+    private var mTypeface = Typeface.create(Typeface.SANS_SERIF, Typeface.NORMAL)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_graph, container, false)
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        setupLineChart()
+        lineChart.data = lineDataWithCount(20, 100f)
     }
+
+    private fun setupLineChart() {
+        lineChart.apply {
+
+            description.isEnabled = false
+            setTouchEnabled(true)
+            isDragEnabled = true
+            isScaleXEnabled = true
+            setPinchZoom(false)
+            setDrawGridBackground(false)
+
+            //データラベルの表示
+            legend.apply {
+                form = Legend.LegendForm.LINE
+                typeface = mTypeface
+                textSize = 11f
+                textColor = Color.BLACK
+                verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
+                horizontalAlignment = Legend.LegendHorizontalAlignment.LEFT
+                orientation = Legend.LegendOrientation.HORIZONTAL
+                setDrawInside(false)
+            }
+
+            //y軸右側の表示
+            axisRight.isEnabled = false
+
+            //X軸表示
+            xAxis.apply {
+                typeface = mTypeface
+                setDrawLabels(false)
+                setDrawGridLines(true)
+            }
+
+            //y軸左側の表示
+            axisLeft.apply {
+                typeface = mTypeface
+                textColor = Color.BLACK
+                setDrawGridLines(true)
+            }
+        }
+    }
+
+    private fun lineDataWithCount(count: Int, range: Float):LineData {
+
+        //val values = mutableListOf<Entry>()
+        Realm.init(context)
+        val mRealm = Realm.getDefaultInstance()
+        val values = mRealm.where(SaveModel::class.java).findAll()
+
+        val entryArray = mutableListOf<Entry>()
+        for ((index, element) in values.withIndex()) {
+            val entry = Entry(index.toFloat(),element.temperature.toFloat())
+            entryArray.add(entry)
+        }
+        // create a dataset and give it a type
+        val yVals = LineDataSet(entryArray, "SampleLineData").apply {
+            axisDependency =  YAxis.AxisDependency.LEFT
+            color = Color.BLACK
+            highLightColor = Color.YELLOW
+            setDrawCircles(false)
+            setDrawCircleHole(false)
+            setDrawValues(false)
+            lineWidth = 2f
+        }
+        val data = LineData(yVals)
+        data.setValueTextColor(Color.BLACK)
+        data.setValueTextSize(9f)
+        return data
+    }
+
 
 }
